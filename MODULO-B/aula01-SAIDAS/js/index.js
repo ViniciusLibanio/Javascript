02859012471696d388d944ce550dd24500c4aa2b
ViniciusLibanio <ciscoliba@gmail.com>
2023-11-30 17:14:01 -0300

    aula 02 declarações

---
+++ MODULO-B/aula01-SAIDAS/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById('texto').innerHTML="Meu primeiro texto <b>JS</b>";
+alert('Isso é um alerta');
+console.log('Oi isso é um console.log')
---
+++ MODULO-B/aula02-DECLARACOES/js/index.js
@@ -0,0 +1,11 @@
+let a, b, c; /**O JAVASCRIPT IGNORA ESPAÇOS EM BRANCO. E ACEITA QUEBRA DE LINHAS NO CÓDIGO*/
+a=5;
+b=6;
+c=7;
+
+/**DECLARAÇÕES EM BLOCO */
+
+function minhaFuncao () {
+    document.getElementsById('corpo').style.backgroundColor = 'blue'
+
+}
