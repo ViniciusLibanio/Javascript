6da0f6badc8d25eafc0883198b517f3df89cac15
ViniciusLibanio <142766259+ViniciusLibanio@users.noreply.github.com>
2024-01-05 00:50:58 -0300

    finalização do FOR
    finalizando FOR e criando aula12

--- MODULO-B/aula11-FOR/js/index.js
+++ MODULO-B/aula11-FOR/js/index.js
@@ -12,10 +12,17 @@ dentro do for, utiliza-se 3 parâmetros:
   document.getElementById("teste").innerHTML += i + ", ";
 } */
 
-var ano = new Date().getFullYear();
+/* var ano = new Date().getFullYear();
 
 for (let i = ano; i >= 1900; i--) {
   document.getElementById(
     "ano"
   ).innerHTML += `<option value= ${i}> ${i} </option>`;
+} */
+
+const carros = ["Gol", "Fusca", "Brasília", "Del Rey", "Chevette"];
+var tamanho = carros.length;
+
+for (let i = 0; i < tamanho; i++) {
+  document.getElementById("teste").innerHTML += carros[i] + ", ";
 }
